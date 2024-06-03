fbee84412c957b2aa4d04014728f84d044be54a5
zhaowcheng <zhaowcheng@163.com>
2024-06-03 17:52:38 +0800

    add: 添加单元测试执行脚本 tests/run.py。

---
+++ tests/run.py
@@ -0,0 +1,13 @@
+"""
+单元测试执行脚本。
+"""
+
+import unittest
+
+from pathlib import Path
+
+
+if __name__ == '__main__':
+    startdir = Path(__file__).parent
+    testsuit = unittest.TestLoader().discover(startdir)
+    unittest.TextTestRunner(verbosity=2).run(testsuit)
