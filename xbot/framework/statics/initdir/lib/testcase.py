f4f45e3200d7096efdcd57a345f11cac9fdfbc32
zhaowcheng <zhaowcheng@163.com>
2024-06-30 13:43:02 +0800

    fix: correct the return type of TestCase.testbed in initdir.

--- xbot/framework/statics/initdir/lib/testcase.py
+++ xbot/framework/statics/initdir/lib/testcase.py
@@ -1,8 +1,15 @@
 from xbot.framework import testcase
 
+from .testbed import TestBed
+
 
 class TestCase(testcase.TestCase):
     """
     Implement as needed.
     """
-    pass
+    @property
+    def testbed(self) -> TestBed:
+        """
+        TestBed instance.
+        """
+        return self.__testbed
